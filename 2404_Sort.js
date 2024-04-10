480085a6a2506124fec7805053647f05b44cbee9
joonngy <joonnngy@gmail.com>
2024-04-10 17:46:27 +0900

    Sort: 가장 큰 수

--- 2404_Sort.js
+++ 2404_Sort.js
@@ -12,13 +12,22 @@ function solution(array, commands) {
     return answer;
 }
 
-const input1 = [1, 5, 2, 6, 3, 7, 4];
-const input2 = [
-    [2, 5, 3],
-    [4, 4, 1],
-    [1, 7, 3],
-];
+/* 가장 큰 수 */
+function solution(numbers) {
+    const answer = numbers
+        .map(v => v.toString())
+        .sort((a, b) => b + a - (a + b))
+        .join('');
+    // Number가 지원하는 자리수가 제한적이기 때문에 아래 공식은 성립하지 않음
+    // return Number(answer).toString();
+    return answer[0]==='0'?'0':answer;
+}
+
+// const input1 = [6, 10, 2];
+// const input1 = [3, 30, 34, 5, 9];
+const input1 = [1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000];
+const input2 = [[2, 5, 3]];
 const input3 = [1, 1, 1, 1, 1, 3, 3];
-// console.log(solution(input1));
-console.log(solution(input1, input2));
+console.log(solution(input1));
+// console.log(solution(input1, input2));
 // console.log(solution(input1, input2, input3));
