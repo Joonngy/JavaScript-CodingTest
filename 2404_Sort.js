ab44188ad4aa901355a7e76829158e1e6ab58c70
joonngy <joonnngy@gmail.com>
2024-04-17 19:06:33 +0900

    Sort: H-Index

--- 2404_Sort.js
+++ 2404_Sort.js
@@ -20,12 +20,23 @@ function solution(numbers) {
         .join('');
     // Number가 지원하는 자리수가 제한적이기 때문에 아래 공식은 성립하지 않음
     // return Number(answer).toString();
-    return answer[0]==='0'?'0':answer;
+    return answer[0] === '0' ? '0' : answer;
 }
 
-// const input1 = [6, 10, 2];
-// const input1 = [3, 30, 34, 5, 9];
-const input1 = [1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000];
+/* H-Index */
+function solution(citations) {
+    citations.sort((a, b) => b - a);
+
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] < i) {
+            return i;
+        }
+    }
+
+    return citations.length
+}
+
+const input1 = [0, 0, 0, 0, 0];
 const input2 = [[2, 5, 3]];
 const input3 = [1, 1, 1, 1, 1, 3, 3];
 console.log(solution(input1));
