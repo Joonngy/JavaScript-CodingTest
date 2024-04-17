0712af2aafe2796e2ffd331eb385341dcd68a9d0
joonngy <joonnngy@gmail.com>
2024-04-17 20:46:56 +0900

    ExhaustiveSearch: 모의고사

--- 2404_ExhaustiveSearch.js
+++ 2404_ExhaustiveSearch.js
@@ -14,12 +14,32 @@ function solution(sizes) {
     return firstMax * secondMax;
 }
 
-const input1 = [
-    [60, 50],
-    [30, 70],
-    [60, 30],
-    [80, 40],
-];
+/* 모의고사 */
+function solution(answers) {
+    const answer1 = [1, 2, 3, 4, 5];
+    const answer2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+    let index = 0;
+    const correctAnswer = [0, 0, 0];
+    while (index < answers.length) {
+        if (answer1[index % answer1.length] === answers[index]) correctAnswer[0]++;
+        if (answer2[index % answer2.length] === answers[index]) correctAnswer[1]++;
+        if (answer3[index % answer3.length] === answers[index]) correctAnswer[2]++;
+        index++;
+    }
+
+    index = 0;
+    const maxValue = Math.max(correctAnswer[0], correctAnswer[1], correctAnswer[2]);
+    const returnValue = [];
+    while (index < correctAnswer.length) {
+        if (correctAnswer[index] === maxValue) returnValue.push(index + 1);
+        index++;
+    }
+    return returnValue;
+}
+
+const input1 = [1, 3, 2, 4, 2];
 const input2 = [[2, 5, 3]];
 const input3 = [1, 1, 1, 1, 1, 3, 3];
 console.log(solution(input1));
